50447bbf2fdb8d70e4b1a842577c4f9fdd168ba3
karolmarkut <karolmarkut11@gmail.com>
2026-01-15 19:05:59 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,19 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int):
+        self.name = name
+        self.age = age
+        Person.people[self.name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    new_people = [Person(element["name"], element["age"]) for element in people]
+    for elem in new_people:
+        for element in people:
+            if elem.name == element["name"]:
+                if element.get("wife") is not None:
+                    elem.wife = element.get("wife")
+                if element.get("husband") is not None:
+                    elem.husband = element.get("husband")
+    return new_people
