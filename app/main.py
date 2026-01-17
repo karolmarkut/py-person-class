90ad30e6baf0e7a912035b1c8ea2921d25b0636e
karolmarkut <karolmarkut11@gmail.com>
2026-01-17 04:43:40 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -4,8 +4,6 @@ class Person:
     def __init__(self, name: str, age: int):
         self.name = name
         self.age = age
-        self.wife = None
-        self.husband = None
         Person.people[self.name] = self
 
 
@@ -13,11 +11,13 @@ def create_person_list(people: list) -> list:
     new_people = [Person(element["name"],
                          element["age"]) for element in people]
     for element in people:
-        obiekt = Person.people[element["name"]]
-        if "wife" in element:
-            zona = element["wife"]
-            obiekt.wife = Person.people.get(zona)
-        if "husband" in element:
-            maz = element["husband"]
-            obiekt.husband = Person.people.get(maz)
+        current_person = Person.people[element["name"]]
+        wife_name = element.get("wife")
+        if wife_name:
+            wife_name = element["wife"]
+            current_person.wife = Person.people.get(wife_name)
+        husband_name = element.get("husband")
+        if husband_name:
+            husband_name = element["husband"]
+            current_person.husband = Person.people.get(husband_name)
     return new_people
